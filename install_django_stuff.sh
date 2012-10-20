eeff5aa0b4911800bd6bc943938aca55f5a05064
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-10-20 15:14:06 +0100

    Use requirements.txt to specify dependencies

--- install_django_stuff.sh
+++ install_django_stuff.sh
@@ -1,20 +1,5 @@
 # Install the requirements
-pip install psycopg2
-pip install pillow
-pip install django-celery
-pip install kombu==2.4.3
-pip install pytz
-pip install django-admin-tools
-pip install South
-pip install django-mailer
-pip install django-registration
-pip install django_extensions
-pip install django_nose
-pip install django_mobile
-pip install django-crispy-forms
-pip install django-generic-m2m
-pip install django-sendsms
-pip install django-sekizai
+pip install -r requirements.txt
 
 python djangorifa/manage.py syncdb --all
 python djangorifa/manage.py migrate --fake
